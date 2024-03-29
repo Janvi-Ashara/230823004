892a6285f769b0e6c88299474d9c70f00616baec
Janvi-Ashara <143256436+Janvi-Ashara@users.noreply.github.com>
2024-03-29 08:47:14 +0530

    Delete src directory

--- src/component/Navbaritem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Navbaritem(props) {
-    return (
-        <>
-            <p>{props.icon}<span style={{ marginLeft: '20px' }}>{props.text}</span></p>
-        </>
-    )
-}
